18f0ff12147007d3806cb4a0c9e752493b442fb8
cat <tanasohlo.yurii@gmail.com>
2024-10-03 10:44:07 +0300

    Added required comments

--- homework_3/homework_3_1.py
+++ homework_3/homework_3_1.py
@@ -4,6 +4,8 @@ first_operand = int(input("Please enter the first operand in format N+ (e.g., 12
 second_operand = int(input("Please enter the second operand in format N+ (e.g., 123, 837789, 1): "))
 operand = input("Please enter the operator the options (+, -, *, /): ")
 
+# Processing the input data by using match statement
+# In each case assigning result of the operation to the result variable
 match operand:
     case "+":
         result = first_operand + second_operand
@@ -12,10 +14,13 @@ match operand:
     case "*":
         result = first_operand * second_operand
     case "/":
+        # In case of divide operator check that the second operand is not 0
         if second_operand == 0:
             result = "Cannot be divided by 0!"
         else:
             result = first_operand / second_operand
     case _:
         result = "Unexpected error.."
+
+# Printing the result
 print(f"The result is: {result}")
