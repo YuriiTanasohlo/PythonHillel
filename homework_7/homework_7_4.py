d6bf7a6db6833ef2d003d5a701659cfe10174be7
cat <tanasohlo.yurii@gmail.com>
2024-10-17 00:14:32 +0300

    Added more comments

--- homework_7/homework_7_4.py
+++ homework_7/homework_7_4.py
@@ -1,8 +1,9 @@
 def common_elements():
-    # Generate two lists
+    # Generate two lists according to the requirements
     numbers_multiples_of_3 = list(range(0, 100, 3))
     numbers_multiples_of_5 = list(range(0, 100, 5))
 
+    # Convert the lists to sets and return numbers that exist in the both sets
     return set(numbers_multiples_of_3).intersection(set(numbers_multiples_of_5))
 
 
