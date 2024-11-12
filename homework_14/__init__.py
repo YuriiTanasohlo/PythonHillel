83fdc86852e9de7b72c10dd473db9d18b559e636
cat <tanasohlo.yurii@gmail.com>
2024-11-12 21:47:44 +0200

    Completed HW 14.2

--- homework_14/__init__.py
+++ homework_14/__init__.py
@@ -1,40 +1,19 @@
-import traceback
-import copy
-
 from homework_14.model.student import Student
 from homework_14.model.group import Group
 
-from homework_14.exceptions.group_exception import OverloadedGroupException
 
-group_1 = Group("1")
-vasyl = Student("Vasyl", "Vasylenko", Student.Gender.MALE, 23, "23234")
-ivan = Student("Ivan", "Ivanenko", Student.Gender.MALE, 22, "23235")
-petro = Student("Petro", "Petrenko", Student.Gender.MALE, 23, "23236")
+if __name__ == "__main__":
 
-group_1.add_student(vasyl)
-group_1.add_student(ivan)
+    group_1 = Group("1")
+    vasyl = Student("Vasyl", "Vasylenko", Student.Gender.MALE, 23, "23234")
+    ivan = Student("Ivan", "Ivanenko", Student.Gender.MALE, 22, "23235")
+    petro = Student("Petro", "Petrenko", Student.Gender.MALE, 23, "23236")
+    petro1 = Student("Petro", "Petrenko", Student.Gender.MALE, 23, "23236")
 
-print(group_1)
-print(group_1.find_student(vasyl.get_last_name()))
-print(group_1.find_student(ivan.get_last_name()))
-print(group_1.find_student(petro.get_last_name()))
-
-group_1.delete_student(vasyl.get_last_name())
-group_1.delete_student(petro.get_last_name())
-
-
-# check OverloadedGroupException exception
-
-# generate multiple students using lib copy
-for _ in range(10):
-    student_copy = copy.copy(petro)
-    group_1.add_student(student_copy)
-
-print(group_1.length())
-try:
+    group_1.add_student(vasyl)
     group_1.add_student(ivan)
-except OverloadedGroupException as oge:
-    print(oge)
-    # print stacktrace using lib traceback
-    print(traceback.format_exc())
+    group_1.add_student(petro)
 
+    print(group_1.find_student("Petrenko") == petro)
+    print(group_1.find_student("Petrenko") == petro1)
+    print(group_1.find_student("Petrenko") == ivan)
--- homework_14/model/person.py
+++ homework_14/model/person.py
@@ -2,17 +2,47 @@ from enum import Enum
 
 
 class Person:
+    __first_name: str
+    __last_name: str
+    __gender: "Person.Gender"
+    __age: int
+
     def __init__(self, first_name: str, last_name: str, gender: "Person.Gender", age: int):
         self.__first_name = first_name
         self.__last_name = last_name
         self.__gender = gender
         self.__age = age
 
+    @property
+    def first_name(self):
+        return self.__first_name
+
+    @first_name.setter
+    def first_name(self, first_name):
+        self.__first_name = first_name
+
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    @last_name.setter
+    def last_name(self, last_name):
+        self.__last_name = last_name
+
     def get_last_name(self):
         return self.__last_name
 
     def __str__(self):
         return f"Person: first_name = {self.__first_name}, last_name = {self.__last_name}"
+
+    def __eq__(self, other: "Person"):
+        if self.last_name == other.last_name and self.first_name == other.first_name:
+            return True
+        else:
+            return False
+
+    def __hash__(self):
+        return hash(str(self))
 
     class Gender(Enum):
         MALE = "male"
